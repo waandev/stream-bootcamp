44ac9e09195e59b338fd034e09fe03bfd9c454bb
Muhammad Aswan <muhaswan190897@gmail.com>
2022-08-21 16:03:12 +0800

    separate routes to difference files

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\MovieController;
 use App\Http\Controllers\Admin\LoginController;
-use App\Http\Controllers\Admin\TransactionController;
 
 use App\Http\Controllers\Member\RegisterController;
 use App\Http\Controllers\Member\LoginController as MemberLoginController;
 use App\Http\Controllers\Member\PricingController;
-use App\Http\Controllers\Member\DashboardController as MemberDashboardController;
-use App\Http\Controllers\Member\MovieController as MemberMovieController;
-use App\Http\Controllers\Member\TransactionController as MemberTransactionController;
 use App\Http\Controllers\Member\WebhookController;
-use App\Http\Controllers\Member\UserPremiumController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,7 +19,7 @@ use App\Http\Controllers\Member\UserPremiumController;
 |
 */
 
-// Define member routes here
+// describe your public routes here
 Route::view('/', 'index');
 
 Route::get('/pricing', [PricingController::class, 'index'])->name('pricing');
@@ -41,26 +35,5 @@ Route::post('/payment-notification', [WebhookController::class, 'handler'])
 
 Route::view('/payment-finish', 'member.payment-finish')->name('member.payment.finish');
 
-Route::group(['prefix' => 'member', 'middleware' => ['auth']], function () {
-    Route::get('/', [MemberDashboardController::class, 'index'])->name('member.dashboard');
-    Route::get('movie/{id}', [MemberMovieController::class, 'show'])->name('member.movie.detail');
-    Route::get('movie/{id}/watch', [MemberMovieController::class, 'watch'])->name('member.movie.watch');
-
-    Route::post('transaction', [MemberTransactionController::class, 'store'])->name('member.transaction.store');
-
-    Route::get('subscription', [UserPremiumController::class, 'index'])->name('member.user_premium.index');
-    Route::delete('subscription/{id}', [UserPremiumController::class, 'destroy'])->name('member.user_premium.destroy');
-
-    Route::get('logout', [MemberLoginController::class, 'logout'])->name('member.login.logout');
-});
-
-// Define admin routes here
 Route::get('/admin/login', [LoginController::class, 'index'])->name('admin.login');
 Route::post('/admin/login', [LoginController::class, 'authenticate'])->name('admin.login.auth');
-
-Route::group(['prefix' => 'admin', 'middleware' => ['admin.auth']], function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
-    Route::get('/logout', [LoginController::class, 'logout'])->name('admin.login.logout');
-    Route::resource('movie', MovieController::class)->names('admin.movie');
-    Route::get('/transaction', [TransactionController::class, 'index'])->name('admin.transaction');
-});
